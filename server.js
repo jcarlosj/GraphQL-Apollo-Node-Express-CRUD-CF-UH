e4ee19a96cc053b7ef8578d019873a60f7fe7e1e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-04-26 09:50:30 -0500

    [+] Apollo/GraphQL: Lanza servidor/Define Schema
    Detalles
     - Define Schema/Properties (Type Definitions)
     - Crea Schema usando los Type Definitions
     - Asocia Instancia de Apollo Server al Schema
     - Lanzar Servidor Apollo

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-console .log( `File 'server.js' is here!` );
+const 
+    /** Dependencies */
+    { ApolloServer } = require( 'apollo-server' ),      
+    { makeExecutableSchema } = require( 'graphql-tools' ),
+    /** Type Definitions */
+    typeDefs = `
+        type Course {
+            id: ID!
+            title: String!
+            views: Int
+        }
+        type Query {
+            getCourses( page: Int, limit: Int = 1 ) : [ Course ]
+        }
+    `,
+    /** Crea Schema */
+    mySchema = makeExecutableSchema({
+        typeDefs: typeDefs,
+        resolvers: {}
+    }),
+    /** Asocia Instancia de Apollo Server con el Schema */
+    server = new ApolloServer({                         
+        schema: mySchema                                    // Schema
+    });
+
+/** Lanza Servidor */
+server .listen() .then( ({ url }) => {
+    console .log( `Servidor Apollo iniciado en ${ url }`)
+});
